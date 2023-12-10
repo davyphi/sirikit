db76eaaae1b90170989698f100fca6db1eb79bbc
davyphi <davy.philippe@icloud.com>
2023-12-10 23:07:52 +0100

    Initial Commit

---
+++ sirikit/sirikitApp.swift
@@ -0,0 +1,17 @@
+//
+//  sirikitApp.swift
+//  sirikit
+//
+//  Created by davy philippe on 10/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct sirikitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
